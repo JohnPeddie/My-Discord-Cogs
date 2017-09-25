9faa484fad8374d48aacdba0230c968b2311a519
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2017-09-25 15:10:36 +0200

    Update invest.py

--- invest/invest.py
+++ invest/invest.py
@@ -1,23 +1,54 @@
 import discord
 from discord.ext import commands
 
+marketcap = 20000
+balance = 1000
+shareprice =10
+totalshares =2000
+shares=0
+
 class invest:
     """In theory this is an acurate stock sim"""
 
     def __init__(self, bot):
         self.bot = bot
-
+    
     @commands.command()
     async def buy(self, ammount):
         """this is legit only to see if the thing works and doesnt do anything"""
-
+        global marketcap
+        global balance
+        global shares
+        global shareprice
+        global totalshares           
+        
+        shares = shares+amount
+        marketcap += (shareprice*amount)
+        shareprice += (amount/totalshares)*shareprice        
         
         
-        await self.bot.say(("you just purchased {} shares").format(ammount))
+        await self.bot.say("you just purchased {} shares".format(ammount))
         
     @commands.command()
     async def sell(self, ammount): 
-        await self.bot.say(("you just sold {} shares").format(ammount))
+        global marketcap
+        global balance
+        global shares
+        global shareprice
+        global totalshares           
+        
+        shares = shares+amount
+        marketcap += (shareprice*amount)
+        shareprice += (amount/totalshares)*shareprice         
+        
+        
+        await self.bot.say("you just sold {} shares".format(ammount))
+        
+    @commands.command()
+    async def stats(self): 
+        global shareprice
+        
+        await self.bot.say("shareprice = {}".format(shareprice))    
 
 def setup(bot):
     bot.add_cog(invest(bot))
