ecfdd7bb538c614023e735235e455b60661fc4bd
YGDFLAZ <32245064+YGDFLAZ@users.noreply.github.com>
2017-09-25 13:55:04 +0200

    Create invest.py

---
+++ invest/invest.py
@@ -0,0 +1,15 @@
+class invest:
+    """In theory this is an acurate stock sim"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def buy(self, ammount):
+        """this is legit only to see if the thing works and doesnt do anything"""
+
+        #Your code will go here
+        await self.bot.say("you just baught", ammount)
+
+def setup(bot):
+    bot.add_cog(invest(bot))
